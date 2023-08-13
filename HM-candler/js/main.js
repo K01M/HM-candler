7ed9a78c866a13f24e3127d7ddf56f5afd5dae8a
K01M <michail.kasyanov@yandex.ru>
2023-08-13 19:45:54 +0500

    added sections rews and certificates and adaptive styles

--- HM-candler/js/main.js
+++ HM-candler/js/main.js
@@ -4,3 +4,27 @@ const menuMobile = document.querySelector('.header__menu-list');
 menuBtn.addEventListener('click', ()=>{
     menuMobile.classList.toggle('menu--open');
 })
+
+const swiperOne = new Swiper('.rews__slider', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    breakpoints: {
+        480: {
+            slidesPerView: 2,
+        },
+        570: {
+            slidesPerView: 3,
+        },
+    },
+});
